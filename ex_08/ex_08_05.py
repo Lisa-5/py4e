63abfea758c583dcc390e53a30567b7bdb47e48f
Lisa <84411057+Lisa-5@users.noreply.github.com>
2024-07-03 21:43:37 -0500

    wip

--- ex_08/ex_08_05.py
+++ ex_08/ex_08_05.py
@@ -1,20 +1,16 @@
-# fname = input("Enter file name: ")
-# if len(fname) < 1:
-#     fname = "mbox-short.txt"
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
 
-# fh = open(fname)
-# email_lst = list()
+fh = open(fname)
+email_lst = list()
 
-# for line in fh:
-#     line = line.rstrip()
-#     if not line.startswith('From '):
-#         continue
-#     words = line.split()
-#     email_lst.append(words[1])
-#     print(words[1])
+for line in fh:
+    line = line.rstrip()
+    if not line.startswith('From '):
+        continue
+    words = line.split()
+    email_lst.append(words[1])
+    print(words[1])
 
-# print(f'There were {len(email_lst)} lines in the file with From as the first word')
-
-fruit = 'Banana'
-fruit[0] = 'b'
-print(fruit)
+print(f'There were {len(email_lst)} lines in the file with From as the first word')
